6786ced7eb10225411a7928c93e1e8a64119cddb
Suraj Dudhe <119352724+surajdudhe7@users.noreply.github.com>
2024-01-31 00:52:04 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -123,3 +123,11 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+/*import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);*/
